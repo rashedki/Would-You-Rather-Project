99dd00b76d5e27d930b0cfb8f511194894cd3970
Khalil <rashedki@hotmail.com>
2019-11-10 14:47:41 +0300

    updating the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
     return (
-      <div>
-        <div>Footer</div>
+      <div
+        className="ui inverted vertical footer segment"
+        style={{ marginTop: "3em", padding: "3em 0" }}
+      >
+        <div className="ui container">
+          <strong>Would You Rather App,</strong> Developed by Khalil Al-Rashid @2019
+        </div>
       </div>
     );
   }
 }
-export default Footer
+
+export default Footer;

101b224c865e45e94cf0ed9c9ed49d775eaa0f69
Khalil <rashedki@hotmail.com>
2019-11-11 21:09:33 +0300

    Implementing the Error Messages to login & newquestion components.
    Issue: the error message appears as popup.
    Solution: update the script to show the error message as message in the same page.

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,10 +1,14 @@
 import React, {Component} from "react"
 import { connect } from "react-redux"
-import { Button, Card, Form, Image, Input } from "semantic-ui-react"
+import { Button, Card, Form, Image, Input, Message } from "semantic-ui-react"
 import { handleAddQuestion } from "../actions/questions"
 
 class NewQuesiton extends Component {
-  state = { optionOne: "", optionTwo: "" };
+  state = {
+    optionOne: "",
+    optionTwo: "",
+    message: { hidden: true, content: "" }
+   };
 
   handleOnChange = (e, data) => {
     this.setState({ [data.id]: data.value });
@@ -15,7 +19,21 @@ class NewQuesiton extends Component {
     const { authedUser: author, history } = this.props;
 
     if (!optionOneText || !optionTwoText) {
-      return alert("Please enter both options");
+      //return alert("Please enter both options");
+      this.setState({
+        message: {
+          hidden: false,
+          content: "Please enter the two Options. Option One as a Text and Option Two as a Text."
+        }
+      });
+      return;
+    } else {
+      this.setState({
+        message: {
+          hidden: true,
+          content: ""
+        }
+      });
     }
     const success = await this.props.handleAddQuestion({
       optionOneText,
@@ -31,44 +49,49 @@ class NewQuesiton extends Component {
     const {authedUser, users} = this.props;
     const user = users[authedUser];
     return (
-      <Card.Group centered>
-        <Card>
-          <Card.Content>
-            <Image floated="right" size="mini" src={user.avatarURL} />
-            <Card.Header>{user.name}</Card.Header>
-            <Card.Meta>Would You Rather</Card.Meta>
-            <Card.Description>
-              <Form>
-                <Form.Field>
-                  <Input
-                    size="mini"
-                    id="optionOne"
-                    placeholder="Enter Option One Text Here"
-                    value={this.state.optionOne}
-                    onChange={this.handleOnChange}
-                  />
-                </Form.Field>
-                <Form.Field>
-                  <Input
-                    size="mini"
-                    id="optionTwo"
-                    placeholder="Enter Option One Text Here"
-                    value={this.state.optionTwo}
-                    onChange={this.handleOnChange}
-                  />
-                </Form.Field>
-              </Form>
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <div className="ui two buttons">
-              <Button basic color="black" onClick={this.handleClick}>
-                Submit
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-      </Card.Group>
+      <div>
+        <Card.Group centered>
+          <Card>
+            <Card.Content>
+              <Image floated="right" size="mini" src={user.avatarURL} />
+              <Card.Header>{user.name}</Card.Header>
+              <Card.Meta>Would You Rather</Card.Meta>
+              <Card.Description>
+                <Form>
+                  <Form.Field>
+                    <Input
+                      size="mini"
+                      id="optionOne"
+                      placeholder="Enter Option One As a Text Here"
+                      value={this.state.optionOne}
+                      onChange={this.handleOnChange}
+                    />
+                  </Form.Field>
+                  <Form.Field>
+                    <Input
+                      size="mini"
+                      id="optionTwo"
+                      placeholder="Enter Option Two As a Text Here"
+                      value={this.state.optionTwo}
+                      onChange={this.handleOnChange}
+                    />
+                  </Form.Field>
+                  <Message hidden={this.state.message.hidden} negative>
+                    {this.state.message.content}
+                  </Message>
+                </Form>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui two buttons">
+                <Button basic color="black" onClick={this.handleClick}>
+                  Submit
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </div>
     );
   }
 }
